91b2be767b6b943d2c6c52350901221ad88f9cb1
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-23 21:15:31 +0200

    saved the scraped data into csv file

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import requests
 from io import BytesIO
 from pdfminer.high_level import extract_text
+import csv
 
 # URL of the PDF file to download
 pdf_url = "https://www.rcsdk12.org/cms/lib04/NY01001156/Centricity/Domain/4194/english-kinyarwanda-dictionary.pdf"
@@ -12,5 +13,13 @@ pdf_buffer = BytesIO(response.content)
 # Extract the text from the PDF file using pdfminer
 text = extract_text(pdf_buffer)
 
-# Print the extracted text to the console
-print(text)
+# Split the text into rows based on newlines
+rows = text.split('\n')
+
+# Open a new CSV file for writing
+with open('english-kinyarwanda-dictionary.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    writer = csv.writer(csvfile)
+    
+    # Write each row of the text to the CSV file
+    for row in rows:
+        writer.writerow([row])
