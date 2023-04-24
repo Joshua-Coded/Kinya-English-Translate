e10aa678930b295e6f40ec0b84a683e84cfef999
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-04-24 15:01:33 +0200

    test_model

--- data/test_model.py
+++ data/test_model.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from transformers import MarianMTModel, MarianTokenizer
+from transformers import TFMarianMTModel, MarianTokenizer
 
 # Load the preprocessed data
 df = pd.read_csv('preprocessed_data.csv')
@@ -8,21 +8,21 @@ df = pd.read_csv('preprocessed_data.csv')
 # Split the data into training and validation sets
 train_df, val_df = train_test_split(df, test_size=0.1, random_state=42)
 
-# Load the MarianMTModel and MarianTokenizer
+# Load the TFMarianMTModel and MarianTokenizer
 model_name = 'Helsinki-NLP/opus-mt-rw-en'
 tokenizer = MarianTokenizer.from_pretrained(model_name)
-model = MarianMTModel.from_pretrained(model_name)
+model = TFMarianMTModel.from_pretrained(model_name)
 
 # Define the function for translating text
 def translate_text(text, max_length=512):
     # Tokenize the text
-    inputs = tokenizer(text, return_tensors='pt', padding=True, truncation=True, max_length=max_length)
+    inputs = tokenizer(text, return_tensors='tf', padding=True, truncation=True, max_length=max_length)
 
     # Generate the translation
     outputs = model.generate(**inputs)
 
     # Decode the translation
-    translated_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    translated_text = tokenizer.batch_decode(outputs.numpy(), skip_special_tokens=True)
 
     return translated_text[0]
 
